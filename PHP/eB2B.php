7bfc3c7638ed15f7b87a6a4a049ee4ef35c87427
dasnie97 <damiansnieda97@gmail.com>
2022-07-27 14:10:51 +0200

    Logic improved, code clean-up, decided to go with hard coded associative array for conversion, user has to fulfill requirements regarding input string format.

--- PHP/eB2B.php
+++ PHP/eB2B.php
@@ -2,88 +2,169 @@
 
 class PhoneKeyboardConverter{
 
+    #region Private fields
+
     private $Output;
+    private $Keyboard = array(
+        2 => "A",
+        22 => "B",
+        222 => "C",
+        3 => "D",
+        33 => "E",
+        333 => "F",
+        4 => "G",
+        44 => "H",
+        444 => "I",
+        5 => "J",
+        55 => "K",
+        555 => "L",
+        6 => "M",
+        66 => "N",
+        666 => "O",
+        7 => "P",
+        77 => "Q",
+        777 => "R",
+        7777 => "S",
+        8 => "T",
+        88 => "U",
+        888 => "V",
+        9 => "W",
+        99 => "X",
+        999 => "Y",
+        9999 => "Z",
+        0 => " ",
+    );
 
-    function __construct($Input)
-    {
-        $Sanitized = $this->filterInput($Input);
-        $Converted = $this->convertInput($Sanitized);
+    #endregion
 
-        $this->Output = $Converted;
-    }
+    #region Private methods
 
     private function convertToNumeric($Input)
     {
-
+        try {
+            $UpperCaseInput = strtoupper($Input);
+            $Keyboard = array_flip($this->Keyboard);
+            $Converted = "";
+    
+            foreach (str_split($UpperCaseInput) as $char) {
+                $Converted .= $Keyboard[$char].",";
+            }
+            return rtrim($Converted, ",");
+        } catch (\Throwable $th) {
+            echo ($th->getMessage());
+            exit();
+        }
     }
 
     private function convertToString($Input)
     {
+        try {
+            $Splitted = explode(",", $Input);
+            $Output = "";
+    
+            for ($i=0; $i < count($Splitted); $i++) {
+                if (array_key_exists($Splitted[$i], $this->Keyboard)) {
+                    $Output .= $this->Keyboard[$Splitted[$i]];
+                }
+                else{
+                    throw new Exception("Input string invalid! ");
+                }
+            }
+            return strtolower($Output);
+        } catch (\Throwable $th) {
+            echo ($th->getMessage());
+            return null;
+        }
 
     }
 
-    // Leave only alphanumeric chars comma and whitespaces
-    private function filterInput($Input)
-    {
-        $Sanitized = preg_replace("/[^a-zA-Z0-9\s,]+/", "", $Input);
-        return $Sanitized;
-    }
-
+    // Decide in which direction conversion should happen
     private function convertInput($Input)
     {
-        $Letters = preg_replace("/[^a-zA-Z\s]+/", "", $Input);
-        $Digits = preg_replace("/[^0-9,]+/", "", $Input);
+        try
+        {
+            if ($Input == preg_replace("/[^a-zA-Z\s]+/", "", $Input)){
+                return $this->convertToNumeric($Input);
+            }
+            if ($Input == preg_replace("/[^0-9,]+/", "", $Input))
+            {
+                return $this->convertToString($Input);
+            }
+            throw new Exception("Input string not valid! Please check your message.");
+        }
+        catch (Exception $e)
+        {   
+            echo ($e->getMessage());
+            exit();
+        }
     }
+
+    #endregion
+
+    #region Public methods
 
     public function showOutput()
     {
-        echo "Converted message: ".$this->Output."\n";
+        if (!is_null($this->Output))
+        {
+            echo "Converted message: ".$this->Output."\n";
+        }
+    }
+
+    #endregion
+    
+    function __construct($Input)
+    {
+        $this->Output = $this->convertInput($Input);
     }
 }
 
 function welcomeUser()
 {
-    echo "====================\n\n";
-    echo "Welcome to OldPhone message converter! Please enter message to convert it to its old phone keybord representation or vice versa!.\n\n";
-    echo "========== NOTE ==========\n";
-    echo "If you want to convert regular string message to its numeric representation - use only english lower and upper case letters and spacebar.\n";
-    echo "If you want to convert old phone numeric message to its string representation - use only digits separated by commas.\n\n";
-    echo "========== EXAMPLE ==========\n";
-    echo "Ela nie ma kota => 33,555,2,0,66,444,33,0,6,2,0,55,666,8,2\n";
-    echo "5,2,22,555,33,222,9999,66,444,55 => jablecznik\n\n";
+echo <<<END
+====================
+
+Welcome to OldPhone message converter! Please enter message to convert it to its old phone keybord representation or vice versa!.
+
+========== NOTE ==========
+If you want to convert regular string message to its numeric representation - use only english lower and upper case letters and spacebar.
+If you want to convert old phone numeric message to its string representation - use only digits separated by commas.
+
+========== EXAMPLE ==========
+Ela nie ma kota => 33,555,2,0,66,444,33,0,6,2,0,55,666,8,2
+5,2,22,555,33,222,9999,66,444,55 => jablecznik
+
+====================
+
+END;
 }
 
 function getUserInput()
 {
-    while (true)
+    $Input = readline("Enter input: ");
+    $isString = $Input == preg_replace("/[^a-zA-Z\s]+/", "", $Input);
+    $isNumeric = $Input == preg_replace("/[^0-9,]+/", "", $Input);
+    if (($isString || $isNumeric) && $Input != "")
     {
-        $Input = readline("Enter input: ");
-        $Sanitized = preg_replace("/[^a-zA-Z0-9\s,]+/", "", $Input);
-        if ($Sanitized == $Input)
-        {
-            break;
-        }
-        else
-        {
-            $UserResponse = readline("Not allowed character present in input string! Try again? (Y/N): ");
-            if (!str_contains(strtolower($UserResponse), "y"))
-            {
-                echo "Thank you for using OldPhone converter!\n";
-                exit();
-            }
-        }
+        return $Input;
     }
-    return $Input;
+    else
+    {
+        echo "Input string invalid! ";
+        return null;
+    }
 }
 
-
+//==================================================================================
 welcomeUser();
-// Main loop
 while (true)
 {
     $UserInput = getUserInput();
-    $Message = new PhoneKeyboardConverter($UserInput);
-    $Message->showOutput();
+    if (!is_null($UserInput))
+    {
+        $Message = new PhoneKeyboardConverter($UserInput);
+        $Message->showOutput();
+    }
 
     $UserResponse = readline("Wanna try again? (Y/N): ");
     if (!str_contains(strtolower($UserResponse), "y"))
